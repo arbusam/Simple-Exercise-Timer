14018a0cf096d5d12e7130b58c35b17516918cab
arhanbusam <arhan.busam@gmail.com>
2019-03-11 10:58:30 +1100

    Initial Commit

---
+++ Simple Exersice Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Exersice Timer
+//
+//  Created by Arhan Busam on 11/3/19.
+//  Copyright © 2019 Arhan Busam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
